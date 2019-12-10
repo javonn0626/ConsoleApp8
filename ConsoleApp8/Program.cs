0ffdeac27ec720a0582fadf3febb7d768318685f
MELIH MELIH <melihminnet@outlook.com.tr>
2019-12-10 10:13:42 +0300

    deneme

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //github bize depolama alanı sunuyor  depo-repository
+        }
+    }
+}
