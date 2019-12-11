cbe5b0356c2896a021d9c14c86e0e784e9dd0a8b
MELIH MELIH <melihminnet@outlook.com.tr>
2019-12-11 19:38:02 +0300

    deneme

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -11,6 +11,158 @@ namespace ConsoleApp8
         static void Main(string[] args)
         {
             //github bize depolama alanı sunuyor  depo-repository
+
+            //1 DEN 10 KADAR OLAN SAYILARI EKRANA YAZDIRMA
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+            //1 İLE 40 ARASINDAKİ ÇİFT SAYILARIN TOPLAMI
+
+            //int toplam = 0;
+            //for (int i = 1; i <= 40; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        toplam += i;
+            //}
+            //Console.WriteLine(toplam);
+            //Console.ReadKey();
+
+            //KLAVYEDEN GİRİLEN SAYININI FAKTÖRİYELİNİ HESAPLAMA
+
+            //Console.Write("Bir sayı giriniz:");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //int sonuc = 1;
+            //for (int i = sayi; i > 1; i--)
+            //{
+            //    sonuc *= i;
+            //}
+            //Console.Write("Faktöriyeli:" + sonuc);
+            //Console.ReadKey();
+            
+
+            //EKRANA 10 KERE MERHABA DÜNYA YAZDIRMA
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i + "- Merhaba Dünya");
+            //}
+            //Console.ReadKey();
+
+            //ALFABEYİ EKRANA YAZDIRMA
+            //KARAKTER İSMİNİ CHARA TANIMLADIK
+
+            //char karakter;
+            //for (karakter = 'a'; karakter <= 'z'; karakter++)
+            //{
+            //    Console.Write(karakter + " ");
+            //}
+            //Console.ReadKey();
+
+            //1 İLE 1000 ARASINDA 2 İLE 21 BÖLÜNNEN SAYILAR
+            // && VE DEMEK
+
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    if (i % 2 == 0 && i % 21 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }           
+            //}
+            //Console.ReadKey();
+
+            //GİRİLEN İKİ SAYININ TOPLAMI
+            //toint32 BELİRLEDİĞİMİZ DEĞERİ 32 BİTLİK DEĞERE ÇEVİRİR
+
+            //int toplam = 0, a, b;
+
+            //Console.WriteLine("bir sayi girin");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("bir sayi girin");
+            //b = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    toplam += i;
+            //}
+            //Console.WriteLine("Toplam : {0}", toplam);
+            //Console.ReadKey();
+
+            // kullanıcının girdiği 2 sayı arasında 4 bölünenleri bulan 
+
+            //Console.Write("1. Sayıyı girin:");
+            //int sayi1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("2. Sayıyı girin:");
+            //int sayi2 = Convert.ToInt32(Console.ReadLine());
+            //for (int i = sayi1; i <= sayi2; i++)
+            //{
+            //    if (i % 4 == 0)
+            //        Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+            //2 ile 98 arasındaki sayıları 3er 3er artırıp toplama
+
+            //int toplam = 0;
+            //for (int i = 2; i <= 98; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //    toplam = toplam + i;
+            //}
+            //Console.WriteLine("Toplam:{0}", toplam);
+            //Console.ReadKey();
+
+            //klavyede  girilen bir sayıdan küçük olan bütün değerleri gösterir
+
+            //Console.Write("Sayıyı giriniz:");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Aralıktaki sayıları:");
+
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
